47546e8228bd99c82bba8b62912c401a5902ec51
FrankKamala <frankkamala@gmail.com>
2025-02-06 12:23:55 +0300

    Add buttons

--- app/src/main/java/com/example/stagezero/MainActivity.kt
+++ app/src/main/java/com/example/stagezero/MainActivity.kt
@@ -7,16 +7,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.stagezero.ui.theme.StageZeroTheme
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,7 +32,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             StageZeroTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    OpenLinkButton()
+                    OpenLinkButton( )
                 }
             }
         }
@@ -34,9 +41,24 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun GitHubLinkButton(onClick: () -> Unit) {
-    ElevatedButton(onClick = { onClick() }) {
-        Text("Open GitHub Link")
+    Column( modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally) {
+
+        ElevatedButton(onClick = { onClick() }) {
+            Text("Open GitHub Link")
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+        ElevatedButton(onClick = { onClick() }) {
+            Text("Open GitHub Link")
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+        ElevatedButton(onClick = { onClick() }) {
+            Text("Open GitHub Link")
+        }
+
     }
+
 }
 
 @Composable
@@ -45,6 +67,7 @@ fun OpenLinkButton() {
     GitHubLinkButton {
         openLink(context, "https://github.com/FrankKamala/stage0.git")
     }
+
 }
 
 
